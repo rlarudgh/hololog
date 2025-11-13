ce45750ba14718b132a8d577a5586e86a2babad7
rlarudgh <nestjs01@naver.com>
2025-11-13 11:57:34 +0900

    fix: code-rabbit code-review 반영

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT}`}
           crossOrigin="anonymous"
+          strategy="afterInteractive"
         />
       </body>
     </html>
